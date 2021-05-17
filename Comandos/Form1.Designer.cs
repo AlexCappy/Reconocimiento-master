33c7e4afb9f0754a243f73bb8a3c0ac27b273f37
AlexCappy <shair200115@gmail.com>
2021-05-17 13:04:04 -0500

    'pika'

---
+++ Comandos/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Comandos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Empezar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(357, 454);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Palabra:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(482, 454);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "---";
+            // 
+            // Empezar
+            // 
+            this.Empezar.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Empezar.Location = new System.Drawing.Point(586, 442);
+            this.Empezar.Name = "Empezar";
+            this.Empezar.Size = new System.Drawing.Size(147, 55);
+            this.Empezar.TabIndex = 5;
+            this.Empezar.Text = "Empezar";
+            this.Empezar.UseVisualStyleBackColor = true;
+            this.Empezar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Comandos.Properties.Resources.pikachu1;
+            this.pictureBox1.Location = new System.Drawing.Point(403, 226);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(188, 176);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Comandos.Properties.Resources.Pokemon;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1018, 602);
+            this.Controls.Add(this.Empezar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Empezar;
+    }
+}
+
---
+++ Comandos/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using Comandos.Properties;
+using System.Threading.Tasks;
+
+namespace Comandos
+{
+    public partial class Form1 : Form
+    {
+        private SpeechRecognitionEngine escucha = new SpeechRecognitionEngine();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                escucha.SetInputToDefaultAudioDevice();
+                escucha.LoadGrammar(new DictationGrammar());
+                escucha.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(lector);
+                escucha.RecognizeAsync(RecognizeMode.Multiple);
+
+            }
+            catch (InvalidOperationException)
+            {
+                MessageBox.Show("No se puede abrir mas de una vez");
+            }
+        }
+
+        public async void lector(object sender, SpeechRecognizedEventArgs e)
+        {
+            foreach (RecognizedWordUnit palabra in e.Result.Words)
+            {
+                label2.Text = palabra.Text;
+
+                if (palabra.Text == "arriba")
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X - 10, pictureBox1.Location.Y);
+                    pictureBox1.Image = Resources.pikachu3;
+                    await Task.Delay(500);
+                    pictureBox1.Image = Resources.pikachu4;
+                    pictureBox1.Location = new Point(pictureBox1.Location.X - 10, pictureBox1.Location.Y);
+                    await Task.Delay(500);
+                }
+                else if (palabra.Text == "abajo")
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X + 10, pictureBox1.Location.Y);
+                    pictureBox1.Image = Resources.pikachu3;
+                    await Task.Delay(500);
+                    pictureBox1.Image = Resources.pikachu4;
+                    pictureBox1.Location = new Point(pictureBox1.Location.X + 10, pictureBox1.Location.Y);
+                    await Task.Delay(500);
+                   
+                }
+                else if (palabra.Text == "izquierda")
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y-10);
+                    pictureBox1.Image = Resources.pikachu3;
+                    await Task.Delay(500);
+                    pictureBox1.Image = Resources.pikachu4;
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y-10);
+                    await Task.Delay(500);
+                }         
+                else if (palabra.Text == "derecha")
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 10);
+                    pictureBox1.Image = Resources.pikachu3;
+                    await Task.Delay(500);
+                    pictureBox1.Image = Resources.pikachu4;
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 10);
+                    await Task.Delay(500);
+                }
+                else if (palabra.Text == "salta")
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 60);
+                    pictureBox1.Image = Resources.pikachu3;
+                    await Task.Delay(500);
+                    
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 60);
+                    
+                }
+                else if (palabra.Text == "sentado")
+                {
+                    
+                    pictureBox1.Image = Resources.pikachu1;
+                   
+                }
+            }
+        }
+
+        private async void button1_Click_1(object sender, EventArgs e)
+        {
+
+            
+
+
+
+        }
+
+        private  void button2_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+    }
+}
